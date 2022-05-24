0e8c9c9989f9d7fdd2c64efb5eb3eac6736d3540
Kaush-agarwal <83901630+Kaush-agarwal@users.noreply.github.com>
2022-05-24 18:19:22 +0530

    Update streamlit_app.py
    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 
 streamlit.title("My new healthy diner")
 
+streamlit.header('Breakfast mmenu')
+streamlit.text('Rat droppings')
+streamlit.text('Cockroach Soup')
+streamlit.text('Fly-flakes')
+streamlit.text('normal breakfast')
